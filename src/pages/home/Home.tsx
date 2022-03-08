f0f0dba0d9b1b3e3b71a4959c987fcff69152b58
MartinCba <martinalejandrohernandez1@gmail.com>
2022-03-08 10:37:47 -0300

    testing new ssh key

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Poke-React</h1>
+      <h1>Poke-Dex</h1>
+    </>
+  );
+}
+
+export default Home;
